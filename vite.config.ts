21777defc70df3b29a53d9027b881823064a930d
Dima <pyts97@mail.ru>
2024-06-13 18:02:22 +0300

    changed the paths of images and icons

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@@": resolve(__dirname, "public"),
+    },
+  },
   base: "/weather-forecast",
 });
