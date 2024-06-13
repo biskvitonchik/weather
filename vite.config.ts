182eae4108a00bfa1f9024c90cc49163e10ec7b4
Dima <pyts97@mail.ru>
2024-06-13 18:35:46 +0300

    deleted a non-working alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@@": resolve(__dirname, "public"),
-    },
-  },
   base: "/weather-forecast",
 });
